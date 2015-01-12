d9cdc81ae6cee396cfc1286d1f85492ec624de83
Fantast <greefeet@hotmail.com>
2015-01-12 09:25:05 +0700

    Fix TrussVerify Discrete Member

--- truss2Dverify.m
+++ truss2Dverify.m
@@ -348,8 +348,8 @@ no=no(1);
 % minA=min(crossSection);
 switch PRB.dv.TypeSection
     case TypeSection.Discrete
-        maxA=max(dv.crossSection(:,1));
-        minA=min(dv.crossSection(:,1));
+        maxA=max(PRB.dv.crossSection(:,1));
+        minA=min(PRB.dv.crossSection(:,1));
     case TypeSection.Continuous
         maxA=PRB.dv.sectionMax;
         minA=PRB.dv.sectionMin;
